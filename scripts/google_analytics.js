6765304107e3bdee47bd49f81a74056f5565aef3
Andrew Seigner <andrews@twitter.com>
2011-12-20 00:55:31 -0800

    updated google analytics and api keys

--- scripts/google_analytics.js
+++ scripts/google_analytics.js
@@ -1,11 +1,9 @@
-<!--
-		
-	document.write(
-		'<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"><\/script>' +
-		'<script type="text/javascript">' +
-			'_uacct = "UA-1350681-1";' +
-			'urchinTracker();' +
-		'<\/script>'
-	);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-27834075-1']);
+_gaq.push(['_trackPageview']);
 
-//-->
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
--- scripts/google_api_keys.js
+++ scripts/google_api_keys.js
@@ -2,7 +2,11 @@
 
 	api_key = '';
 
-	if (location.host.indexOf('siggy.homelinux.org') != -1)
+	if (location.host.indexOf('sig.gy') != -1)
+	{
+		api_key = 'ABQIAAAAUgG3WWCNNvXeS1ya5LxSFRSHpiyV69Wi-hWbOq8W1jb6DZTUfRRRUmF8lRlf-to89zCejgtaF59jmA';
+	}
+	else if (location.host.indexOf('siggy.homelinux.org') != -1)
 	{
 		api_key = 'ABQIAAAAUgG3WWCNNvXeS1ya5LxSFRQIsgCefr63bl9LVl-2Y5vEJZqgLhQ8gucbba1KuVaN9XdGF_AqjgbQng';
 	}
