c70e16f4cf0f3b37018a4946503456f982134e20
Andrew Seigner <andrew@sig.gy>
2019-11-09 14:42:21 -0800

    fix service worker cache name

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('airhorner').then(function(cache) {
+    caches.open('siggy').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html'
